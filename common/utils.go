b1dfdf3c25ef963d3512b7452bf485d550fc3c3e
Pau <paurosello@gmail.com>
2022-11-01 09:59:26 +0100

    Add common function

---
+++ common/utils.go
@@ -0,0 +1,20 @@
+package common
+
+import (
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/micro/micro/v3/service/config"
+)
+
+func get_ethclient(network string) (*ethclient.Client, error) {
+	web3URL, err := config.Get("web3." + network)
+	if err != nil {
+		return nil, err
+	}
+
+	ethclient, err := ethclient.Dial(web3URL.String(""))
+	if err != nil {
+		return nil, err
+	}
+
+	return ethclient, nil
+}
